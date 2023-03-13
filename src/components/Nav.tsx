eb9e916c234f3cb8e4f2ba45a44529f5d72ea009
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2023-03-13 19:29:14 +0000

    updated nav sizing and placement

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -43,28 +43,28 @@ export const Nav = ()=>{
         }
     return(
         <>
-            <nav className="w-[100vw] h-[50px] bg-[#333333] flex">
-                <div className="flex space-x-2 ml-5 pt-[5px] text-gray-200 hover:cursor-pointer hover:text-gray-400 space-between">
+            <nav className="w-[100vw] h-[60px] bg-[#333333] flex">
+                <div className="flex space-x-2 ml-5 text-gray-200 hover:cursor-pointer hover:text-gray-400 space-between self-center">
                     <img src={logo} alt="" className='w-[35px] h-[35px]'/>
                     <p className='font-bold text-[25px] font-["Itim"]'>Crepz</p>
                 </div>
                 {
                     !mobileMenu?(
                         <>
-                            <div className='flex space-x-5 w-[230px] mx-auto text-gray-200 font-bold pt-[10px]'>
+                            <div className='flex space-x-5 w-[230px] mx-auto text-gray-200 font-bold self-center'>
                                 <p className='hover:cursor-pointer hover:text-gray-400'>Men</p>
                                 <p className='hover:cursor-pointer hover:text-gray-400'>Women</p>
                                 <p className='hover:cursor-pointer hover:text-gray-400'>Kids</p>
                                 <p className='hover:cursor-pointer hover:text-gray-400'>Sale</p>
                             </div>
-                            <div className='flex space-x-3 mt-[7px] mr-5'>
+                            <div className='flex space-x-3 mt-[7px] mr-5 self-center -mt-[5px]'>
                                 <img src={favorites} alt="" className='w-[30px] h-[30px] hover:cursor-pointer hover:brightness-50'/>
                                 <img src={userIcon} alt="" className='w-[30px] h-[30px] hover:cursor-pointer hover:brightness-50'/>
                                 <img src={basket} alt="" className='w-[30px] h-[30px] hover:cursor-pointer hover:brightness-50'/>
                             </div>
                         </>
                     ):(
-                        <div className='w-[100%] flex place-content-end	'>
+                        <div className='w-[100%] flex place-content-end	self-center -mt-[5px]'>
                             <img src={basket} alt="" className='w-[40px] h-[40px] hover:cursor-pointer hover:brightness-50 mr-5 mt-[3px]'/>
                             <img src={mobileMenuIcon} alt="" onClick={()=> setShowMobileMenu(true)} className='w-[30px] h-[30px] mr-5 mt-[10px] hover:cursor-pointer'/>
                         </div>

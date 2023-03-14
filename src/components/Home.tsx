47b2a053b72b1aa77963835c2f19d68c7cc1b981
Mateusz <kroplewskimateusz@gmail.com>
2023-03-14 23:50:56 +0000

    styled shop by brand section

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,16 @@
-import { useState } from 'react'
-import heroVid from '../assets/heroVideo.mp4'
+import { useState } from 'react';
+import heroVid from '../assets/heroVideo.mp4';
+import Nike from '../assets/images/heroShoes/nike.png';
+
+
 export const Home = ()=>{
 
-    const [brands,setBrands] = useState(['Nike','Addidas','Puma', 'Converse', 'New Balance', 'Under Armour']);
+    const [brands,setBrands] = useState(['Nike','Addidas','Puma', 'Converse', 'NewBalance', 'UnderArmour']);
 
+    function getImg(name:string){
+        const img = new URL(`../assets/images/heroShoes/${name}.png`, import.meta.url).href;
+        return img;
+    }
     return (
         <>
             <section className="w-[100%] h-[300px] grid place-content-center">
@@ -23,8 +30,12 @@ export const Home = ()=>{
                 <div className='flex flex-wrap pt-[10px]'>
                     {
                         brands.map((brand)=>(
-                            <div className='w-[300px] h-[200px] rounded-md bg-gray-400 mr-5 mt-5 grid place-content-center'>
-                                <p className='font-bold'>{brand}</p>
+                            <div key={brand} 
+                            className='w-[300px] h-[200px] rounded-md mr-5 mt-5 grid place-content-center relative hover:cursor-pointer hover:scale-[1.2] hover:z-50 duration-100 ease-linear'>
+                                <img src={getImg(brand)} alt="" className='absolute rounded-md w-[100%] h-[100%]'/>
+                                <div className='w-[200px] h-[50px] z-50 text-center rounded-md backdrop-blur-md'>
+                                    <p className='font-bold z-50 text-[30px]'>{brand}</p>
+                                </div>
                             </div>
                         ))
                     }

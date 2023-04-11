6a4545478f5c444bbf28de763532f30ccfe9dab9
Mateusz <kroplewskimateusz@gmail.com>
2023-04-11 23:22:21 +0100

    filter desktop view css added

--- src/Pages/Browse.tsx
+++ src/Pages/Browse.tsx
@@ -3,16 +3,16 @@ import filterIcon from '../assets/images/filter.png'
 import { Filter } from '../components/Filter'
 
 export const Browse = ()=>{
-    const [showFilter, setShowFilter] = useState<boolean>(false);
+    const [showFilter, setShowFilter] = useState<boolean>(true);
     
     const closeFilter = ()=>{
         setShowFilter(false);
     }
 
     return(
-        <section className="w-[100vw] pt-16">
+        <section className="w-[100vw] pb-10 pt-16">
             <div className="w-[200px] mx-auto flex space-x-3">
-                <div className='grid place-content-center w-[95px] h-[40px] bg-[#333333] hover:cursor-pointer' onClick={()=>setShowFilter(true)}>
+                <div className='grid place-content-center w-[95px] h-[40px] bg-[#333333] hover:cursor-pointer md:hidden' onClick={()=>setShowFilter(true)}>
                     <div className='flex space-x-2'>
                         <p className='text-gray-200 '>Filter</p>
                         <img src={filterIcon} alt="" className='w-[20px] h-[20px]' />
@@ -28,7 +28,7 @@ export const Browse = ()=>{
             {
                 showFilter?(<Filter closeFilter={closeFilter}/>):(<></>)
             }
-            
+
         </section>
     )
 }
--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -8,13 +8,15 @@ interface FilterProps{
 
 export const Filter = (props:FilterProps)=>{
 
-    return(
-        <div className="w-[100vw] pb-10 absolute top-0 right-0 bg-[#333333] z-[100]">
-            <img src={close} alt="" onClick={props.closeFilter} className='absolute w-[30px] h-[30px] right-1 top-1'/>
-            <h1 className="text-center font-bold text-gray-200 text-[35px] mt-10">Filter</h1>
+    
 
-            <div className="ml-5 mt-10 text-gray-200 font-bold text-[20px]">
-                <h2 className="text-[30px]">Brand:</h2>
+    return(
+        <div className="w-[100vw] md:w-[300px] pb-10 absolute top-0 right-0 md:relative bg-[#333333] md:bg-gray-200 z-[100] text-gray-200 md:text-[#333333] md:ml-16">
+            <img src={close} alt="" onClick={props.closeFilter} className='absolute w-[30px] h-[30px] right-1 top-1 md:hidden'/>
+            <h1 className="text-center md:text-left font-bold text-[35px] mt-10 md:ml-5">Filter</h1>
+
+            <div className="ml-5 mt-10 md:mt-5 font-bold text-[20px] md:text-[16px]">
+                <h2 className="text-[30px] md:text-[25px]">Brand:</h2>
                 <div className="mt-5 flex flex-col space-y-5">
                     <div className="">
                         <label htmlFor="newBalance" className="mr-[10px]">New Balance: </label>
@@ -42,8 +44,8 @@ export const Filter = (props:FilterProps)=>{
                     </div>
                 </div>
             </div>
-                <hr className="mt-5 w-[90%] mx-auto"/>
-                <div className="ml-5 mt-10 text-gray-200 font-bold text-[20px]">
+                <hr className="mt-5 w-[90%] mx-auto md:border-2 md:border-gray-300"/>
+                <div className="ml-5 mt-10 md:mt-5 font-bold text-[20px]">
                     <h2 className="text-[30px]">Gender:</h2>
                     <div className="mt-5 flex flex-col space-y-5">
                         <div className="">
@@ -60,8 +62,8 @@ export const Filter = (props:FilterProps)=>{
                         </div>
                     </div> 
                 </div>
-                <hr className="mt-5 w-[90%] mx-auto"/>
-                <div className="ml-5 mt-10 text-gray-200 font-bold text-[20px]">
+                <hr className="mt-5 w-[90%] mx-auto md:border-2 md:border-gray-300"/>
+                <div className="ml-5 mt-10 md:mt-5 font-bold text-[20px]">
                     <h2 className="text-[30px]">Price:</h2>
                     <div className="flex space-x-2 mt-5">
                             <label htmlFor="min" className="mr-[10px]">Min: </label>
@@ -74,8 +76,8 @@ export const Filter = (props:FilterProps)=>{
                             <p>$200</p>
                     </div>
                 </div>
-                <div className="mx-auto w-[200px] h-[30px] mt-10 ">
-                    <button className="w-[100%] h-[100%] bg-gray-200 font-bold " onClick={props.closeFilter}>Done</button>
+                <div className="mx-auto w-[200px] h-[30px] mt-10 md:hidden">
+                    <button className="w-[100%] h-[100%] bg-gray-200 font-bold text-[#333333]" onClick={props.closeFilter}>Done</button>
                 </div>
         </div>
     )

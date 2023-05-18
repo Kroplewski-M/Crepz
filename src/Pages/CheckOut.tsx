bbbbe9b6de7ac3e3e85e5330c2ab2855ff5fc790
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2023-05-18 17:50:54 +0100

    added rough design on checkout

--- src/Pages/CheckOut.tsx
+++ src/Pages/CheckOut.tsx
@@ -1,4 +1,5 @@
 import shoe from '../assets/images/NikeLogo.jpg';
+import { Heart } from '../components/SVG/Heart';
 import { useBasketInfo } from '../context/BasketContext';
 export const CheckOut = ()=>{
     const {basketItems,updateQuantity,totalPrice} = useBasketInfo();
@@ -16,64 +17,57 @@ export const CheckOut = ()=>{
                     </tr>
                 </thead>
                 <tbody>
-                    <tr className='bg-gray-200 h-[70px] border-b border-black'>
-                        <td className='flex pt-[10px]'>
-                                <img src={shoe} alt="" className='w-[70px] ml-[5px]'/>
-                                <p className='ml-[10px]'>Nike Air Max 2</p>
-                        </td>
-                        <td>
-                            <p className='text-center'>£300</p>
-                        </td>
-                        <td>
-                            <div className='w-[65px] mx-auto'>
-                                <select name="quantity" id="quantity" value='1' className=''>
-                                    <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                    <option value="4">4</option>
-                                    <option value="5">5</option>
-                                    <option value="6">6</option>
-                                    <option value="7">7</option>
-                                    <option value="8">8</option>
-                                    <option value="9">9</option>
-                                    <option value="10">10</option>
-                                </select>
-                            </div>
-                        </td>
-                        <td>
-                            <p className='text-center'>£300</p>
-                        </td>
-                    </tr>
-                    <tr className='bg-gray-200 h-[70px] border-b border-black'>
-                        <td className='flex pt-[10px]'>
-                                <img src={shoe} alt="" className='w-[70px] ml-[5px]'/>
-                                <p className='ml-[10px]'>Nike Air Max 2</p>
-                        </td>
-                        <td>
-                            <p className='text-center'>£300</p>
-                        </td>
-                        <td>
-                            <div className='w-[65px] mx-auto'>
-                                <select name="quantity" id="quantity" value='1' className=''>
-                                    <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                    <option value="4">4</option>
-                                    <option value="5">5</option>
-                                    <option value="6">6</option>
-                                    <option value="7">7</option>
-                                    <option value="8">8</option>
-                                    <option value="9">9</option>
-                                    <option value="10">10</option>
-                                </select>
-                            </div>
-                        </td>
-                        <td>
-                            <p className='text-center'>£300</p>
-                        </td>
-                    </tr>
+                    {
+                        basketItems.map(product =>(
+                                 <tr className='bg-gray-200 h-[70px] border-b border-black'>
+                                    <td className='flex pt-[10px]'>
+                                        <img src={product.imageUrl} alt="" className='w-[70px] ml-[5px] -rotate-[15deg]'/>
+                                        <p className='ml-[10px]'>{product.name}</p>
+                                    </td>
+                                    <td>
+                                        <p className='text-center'>£{product.price}</p>
+                                    </td>
+                                    <td>
+                                        <div className='w-[65px] mx-auto'>
+                                            <select name="quantity" id="quantity" value={product.quantity} className=''>
+                                                <option value="1">1</option>
+                                                <option value="2">2</option>
+                                                <option value="3">3</option>
+                                                <option value="4">4</option>
+                                                <option value="5">5</option>
+                                                <option value="6">6</option>
+                                                <option value="7">7</option>
+                                                <option value="8">8</option>
+                                                <option value="9">9</option>
+                                                <option value="10">10</option>
+                                            </select>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div className='grid place-content-center'>
+                                            <div className='flex'>
+                                                <p className='text-center mr-5'>£{(product.price * product.quantity).toFixed(2)}</p>
+                                                <Heart width={20} height={20} fill='#333333'/>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                        ))
+                    }
                 </tbody>
             </table>
+            <div>
+                    <div className=''>
+                        <button>Back To Shopping</button>
+                    </div>
+                    <div className='flex flex-row-reverse -mt-[25px]'>
+                        <button>Clear All</button>
+                    </div>
+
+                <p className='font-bold text-[#333333] text-[30px] mt-5'>Total: £{totalPrice()}</p>
+                <button>Continue to checkout</button>
+
+            </div>
         </section>
     )
 }

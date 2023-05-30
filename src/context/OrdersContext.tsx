a51ec2b8fb6863500894c8a28bd7e167b2533685
Mateusz <kroplewskimateusz@gmail.com>
2023-05-30 10:34:20 +0100

    orders context mainly done

--- src/context/OrdersContext.tsx
+++ src/context/OrdersContext.tsx
@@ -1,5 +1,5 @@
 import { createContext, ReactNode, useContext,useEffect,useState } from "react";
-
+import { useUserInfo } from "./UserContext";
 type OrderItem = {
     OrderNo: string,
     Date: string,
@@ -7,9 +7,10 @@ type OrderItem = {
 }
 
 interface OrdersProvider{
-    AddToOrders: ()=> void,
+    AddToOrders: (OrderNo:string,Date:string,Total:number)=> void,
     fetchOrders: ()=> void,
     clearOrders: ()=> void,
+    Orders: OrderItem[],
 }
 interface OrderProviderProps{
     children: ReactNode,
@@ -18,4 +19,31 @@ const OrdersProvider = createContext({} as OrdersProvider)
 
 export const useOrdersInfo = ()=>{
     return useContext(OrdersProvider);
+}
+
+export const OrdersContext = ({children}:OrderProviderProps)=>{
+    const {userInfo} = useUserInfo();
+    const [Orders,setOrders] = useState<OrderItem[]>([]);
+
+    const AddToOrders = (OrderNo:string,Date:string,Total:number)=>{
+        setOrders((currItems)=> [...currItems,{OrderNo,Date,Total}])
+    }
+    
+    const fetchOrders = ()=>{
+        //LINK SUPABSE
+    }
+    useEffect(()=>{
+        clearOrders();
+        if(userInfo.id != ''){
+            fetchOrders();
+        }
+    },[userInfo.id])
+
+    const clearOrders = ()=>{
+        setOrders([]);
+    }
+    
+    return <OrdersProvider.Provider value={{AddToOrders,fetchOrders,clearOrders,Orders}}>
+        {children}
+    </OrdersProvider.Provider>
 }

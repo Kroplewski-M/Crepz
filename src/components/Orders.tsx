9e70b9cdf175d1a29431f962d48b0eb6588552df
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2023-06-09 15:57:27 +0100

    fetching orders in profile page

--- src/components/Orders.tsx
+++ src/components/Orders.tsx
@@ -1,9 +1,17 @@
 import { OrdersCard } from "./OrdersCard"
 import { supabase } from "../supabaseClient"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useUserInfo } from "../context/UserContext"
+
+interface Order{
+    ID:string,
+    Date:string,
+    Total:number,
+}
+
 export const Orders = ()=>{
     const {userInfo} = useUserInfo();
+    const [orders,setOrders] = useState<Order[]>([]);
     useEffect(()=>{
         const fetchOrders = async()=>{
             try{
@@ -11,20 +19,33 @@ export const Orders = ()=>{
                     .from('Orders')
                     .select().eq('UserID',userInfo.id);
                 if(error)throw error;
+                else{
+                    data.map(order=>{
+                        setOrders(currItems=>[...currItems,{ID:order.OrderID,Date:order.Date,Total:(order.Total).toFixed(2)}]);
+                    })
+                }
             }catch(error){
                 console.log('error');
             }
         }   
+        fetchOrders();
     },[]);
 
     return(
         <div className="pl-10">
             <h1 className="font-bold text-gray-200 text-[25px] pt-5">Order history:</h1>
             <div className="mt-5 ">
-                <OrdersCard />
-                <OrdersCard />
-                <OrdersCard />
-
+                {
+                    orders.length>0?(
+                        orders.map(order=>(
+                            <div key={order.ID}>
+                                <OrdersCard OrderNo={order.ID} Date={order.Date} Total={order.Total}/>
+                            </div>
+                        ))
+                    ):(
+                        <p className="font-bold text-gray-200">You havent made any orders!</p>
+                    )
+                }
             </div>
         </div>
     )
--- src/components/OrdersCard.tsx
+++ src/components/OrdersCard.tsx
@@ -1,22 +1,27 @@
+import { useNavigate } from "react-router-dom"
+interface OrderCardProps{
+    OrderNo:string,
+    Date:string,
+    Total:number,
+}
 
-
-export const OrdersCard = ()=>{
-    
+export const OrdersCard = (props:OrderCardProps)=>{
+    const navigate = useNavigate();
 
     return(
-        <div className="w-[80%] md:h-[70px] rounded-md bg-gray-200 hover:cursor-pointer hover:bg-gray-300 flex mt-5">
-            <div className="flex md:flex-row flex-col self-center md:space-x-7">
+        <div className="w-[80%] md:h-[70px] rounded-md bg-gray-200 hover:cursor-pointer hover:bg-gray-300 flex mt-5" onClick={()=>navigate(`/OrderConfirmation/${props.OrderNo}`)}>
+            <div className="flex md:flex-row flex-col self-center md:space-x-7 text-center md:text-left">
                 <div className="flex flex-col pl-5">
                     <p className="text-gray-600">Order No.</p>
-                    <p className="font-bold">#45346535645234</p>
+                    <p className="font-bold text-[12px] md:w-[200px] w-[80%]">#{props.OrderNo}</p>
                 </div>
-                <div className="flex flex-col pl-5">
+                <div className="flex flex-col">
                     <p className="text-gray-600">Date</p>
-                    <p className="font-bold">05/04/2023</p>
+                    <p className="font-bold text-[13px]">{props.Date}</p>
                 </div>
-                <div className="flex flex-col pl-5">
+                <div className="flex flex-col ">
                     <p className="text-gray-600">Total</p>
-                    <p className="font-bold">£108.46</p>
+                    <p className="font-bold text-[13px]">£{props.Total}</p>
                 </div>
             </div>
         </div>
